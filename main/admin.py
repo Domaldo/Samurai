b7cb496b9d3e13175771b124d8ea2284d1c4aa5d
Mateo Burbano <mateo.jbo@gmail.com>
2021-01-19 16:49:31 -0500

    Comentario

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cerrajero, Rating#, Ciudad
-
-# Register your models here.
-
-admin.site.register(Cerrajero)
-admin.site.register(Rating)
-# admin.site.register(Ciudad)
